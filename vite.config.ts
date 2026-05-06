a16518357cf678101c0e30687706e856d50e729e
Thomas Faller <thomasfaller@gmail.com>
2026-05-06 12:05:09 +0100

    chore: update vite to version 8.0.10 and clean up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
     plugins: [
         react(),
-        ghPages(), // Add the GitHub Pages plugin here
     ],
-    base: '/confessio/', // Replace 'confessio' with your repo name
+    base: '/confessio/',
 });
